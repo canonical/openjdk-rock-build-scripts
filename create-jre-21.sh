f93950e13499049008e227cbc86cc6d89641022e
Vladimir Petko <vladimir.petko@canonical.com>
2025-09-09 13:37:52 +1200

    fix: formatting of jlink command

--- create-jre-21.sh
+++ create-jre-21.sh
@@ -17,50 +17,50 @@ chroot ${CRAFT_PART_INSTALL}/ /bin/busybox --install
 JAVA_HOME=usr/lib/jvm/java-21-openjdk-${CRAFT_ARCH_BUILD_FOR}
 rm -rf ${CRAFT_PART_INSTALL}/${JAVA_HOME}
 jlink --add-modules java.base,\
-    java.compiler,\
-    java.instrument,\
-    java.logging,\
-    java.management,\
-    java.management.rmi,\
-    java.naming,\
-    java.net.http,\
-    java.prefs,\
-    java.rmi,\
-    java.scripting,\
-    java.se,\
-    java.security.jgss,\
-    java.security.sasl,\
-    java.smartcardio,\
-    java.sql,\
-    java.sql.rowset,\
-    java.transaction.xa,\
-    java.xml,\
-    java.xml.crypto,\
-    jdk.charsets,\
-    jdk.crypto.cryptoki,\
-    jdk.crypto.ec,\
-    jdk.dynalink,\
-    jdk.httpserver,\
-    jdk.internal.vm.ci,\
-    jdk.internal.vm.compiler,\
-    jdk.internal.vm.compiler.management,\
-    jdk.jdwp.agent,\
-    jdk.jfr,\
-    jdk.jsobject,\
-    jdk.localedata,\
-    jdk.management,\
-    jdk.management.agent,\
-    jdk.management.jfr,\
-    jdk.naming.dns,\
-    jdk.naming.rmi,\
-    jdk.net,\
-    jdk.nio.mapmode,\
-    jdk.sctp,\
-    jdk.security.auth,\
-    jdk.security.jgss,\
-    jdk.unsupported,\
-    jdk.xml.dom,\
-    jdk.zipfs --no-header-files --no-man-pages -G \
+java.compiler,\
+java.instrument,\
+java.logging,\
+java.management,\
+java.management.rmi,\
+java.naming,\
+java.net.http,\
+java.prefs,\
+java.rmi,\
+java.scripting,\
+java.se,\
+java.security.jgss,\
+java.security.sasl,\
+java.smartcardio,\
+java.sql,\
+java.sql.rowset,\
+java.transaction.xa,\
+java.xml,\
+java.xml.crypto,\
+jdk.charsets,\
+jdk.crypto.cryptoki,\
+jdk.crypto.ec,\
+jdk.dynalink,\
+jdk.httpserver,\
+jdk.internal.vm.ci,\
+jdk.internal.vm.compiler,\
+jdk.internal.vm.compiler.management,\
+jdk.jdwp.agent,\
+jdk.jfr,\
+jdk.jsobject,\
+jdk.localedata,\
+jdk.management,\
+jdk.management.agent,\
+jdk.management.jfr,\
+jdk.naming.dns,\
+jdk.naming.rmi,\
+jdk.net,\
+jdk.nio.mapmode,\
+jdk.sctp,\
+jdk.security.auth,\
+jdk.security.jgss,\
+jdk.unsupported,\
+jdk.xml.dom,\
+jdk.zipfs --no-header-files --no-man-pages -G \
     --output ${CRAFT_PART_INSTALL}/${JAVA_HOME}
 
 cd ${CRAFT_PART_INSTALL}
