2a57424491e8d41634fdbbe9de675e565cc36cbf
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-23 15:03:25 +1200

    fix: do not install dependencies
    The dependencies are installed by the rock definition itself

--- create-tomcat-jre-21.sh
+++ create-tomcat-jre-21.sh
@@ -1,17 +1,4 @@
 #!/bin/bash -ex
-chisel cut --release ./ --root ${CRAFT_PART_INSTALL}/ \
-    busybox_bins \
-    libtcnative-1_libs \
-    media-types_data \
-    base-files_base \
-    base-files_chisel \
-    libc6_libs \
-    libgcc-s1_libs \
-    libstdc++6_libs \
-    zlib1g_libs \
-    libpcsclite1_libs \
-    libnss3_libs \
-
 JAVA_HOME=usr/lib/jvm/java-21-openjdk-${CRAFT_ARCH_BUILD_FOR}
 rm -rf ${CRAFT_PART_INSTALL}/${JAVA_HOME}
 jlink --add-modules java.base,\
@@ -67,4 +54,3 @@ for tool in java jfr jrunscript jwebserver keytool rmiregistry; do
     /usr/bin/ln -s --relative ${JAVA_HOME}/bin/${tool} usr/bin/
 done
 
-chroot ${CRAFT_PART_INSTALL}/ /bin/busybox --install
